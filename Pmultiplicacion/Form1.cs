033ffdb76648f8a987139e23c7fb790fb106f93f
Citlali <citlali6soto7@gmail.com>
2022-09-22 22:35:02 -0600

    Agregar archivos de proyecto.

---
+++ Pmultiplicacion/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pmultiplicacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //crear elobjeto  
+            Multip mcion = new Multip();
+            mcion.num1 = int.Parse(txt1.Text);
+            mcion.num2 = int.Parse(txt2.Text);
+
+            txtResult.Text = mcion.Multi().ToString();
+        }
+    }
+}
